48eff5d9151cba9f52676087c845bafe88642310
anotherlifekd <anotherlifekd@gmail.com>
2019-03-29 18:46:59 +0200

    add test_file.py

--- app/files.py
+++ app/files.py
@@ -4,6 +4,18 @@ import argparse
 from app.ask import ask
 
 
+def check_args(args):
+    if args.files_name is not None and args.mod is not None and args.string_for_count is not None \
+            and args.mod == 'c' and args.files_name != 'def' and args.string_for_count != 'def':
+        return True
+    elif args.files_name is not None and args.mod is not None and args.string_for_replace is not None \
+            and args.string_for_search is not None and args.mod == 'r' and args.files_name != 'def' \
+            and args.string_for_search != 'def' and args.string_for_replace != 'def':
+        return True
+    else:
+        return False
+
+
 def files_func(args):
     def file():
         BASE_DIR = os.path.join(os.getcwd(), args.files_name)
@@ -34,18 +46,7 @@ def files_func(args):
             except IsADirectoryError:
                 return 'Is a directory'
 
-    def _check_args():
-        if args.files_name is not None and args.mod is not None and args.string_for_count is not None \
-                and args.mod == 'c' and args.files_name != 'def' and args.string_for_count != 'def':
-            return True
-        elif args.files_name is not None and args.mod is not None and args.string_for_replace is not None \
-                and args.string_for_search is not None and args.mod == 'r' and args.files_name != 'def' \
-                and args.string_for_search != 'def' and args.string_for_replace != 'def':
-            return True
-        else:
-            return False
-
-    if _check_args():
+    if check_args(args):
         return file()
     else:
         return False
@@ -53,41 +54,33 @@ def files_func(args):
 
 def check_files(args):
     while True:
-        if files_func(args):
-            print(files_func(args))
-            args.mod = 'def'
-        elif files_func(args):
+        if check_args(args):
             print(files_func(args))
             args.mod = 'def'
         else:
             mod = input('Which mod do you want to choose? (type C to count / type R to replace '
                         '/ type anything else to exit): ').lower()
             if mod == 'c':
-                try:
-                    direction_count = input('<direction of your file>, <counted string>: ').replace(' ', '').split(',')
-                    args = argparse.Namespace(files_name=direction_count[0], mod=mod,
-                                              string_for_count=direction_count[1], string_for_search='def',
-                                              string_for_replace='def')
-                    print(files_func(args))
+                direction_count = input('<direction of your file>, <counted string>: ').replace(' ', '').split(',')
+                args = argparse.Namespace(files_name=direction_count[0], mod=mod, string_for_count=direction_count[1],
+                                          string_for_search='def', string_for_replace='def')
+                if len(direction_count) > 2:
+                    print('Enter the correct parameters')
                     args.string_for_count = 'def'
-                except IndexError:
-                    print('Enter the correct parameters')
                     continue
+                print(files_func(args))
+                args.string_for_count = 'def'
             elif mod == 'r':
-                try:
-                    direction_replace = input('<direction of your file>, <search string>, <replace string>: ')\
-                        .replace(' ', '').split(',')
-                    print(len(direction_replace))
-                    if len(direction_replace) > 3:
-                        print('Enter the correct parameters')
-                        continue
-                    args = argparse.Namespace(files_name=direction_replace[0], string_for_search=direction_replace[1],
-                                              string_for_replace=direction_replace[2], mod=mod, string_for_count='def')
-                    print(files_func(args))
-                    args.string_for_search = args.string_for_replace = 'def'
-                except IndexError:
+                direction_replace = input('<direction of your file>, <search string>, <replace string>: ')\
+                    .replace(' ', '').split(',')
+                if len(direction_replace) > 3:
                     print('Enter the correct parameters')
+                    args.string_for_search = args.string_for_replace = 'def'
                     continue
+                args = argparse.Namespace(files_name=direction_replace[0], string_for_search=direction_replace[1],
+                                          string_for_replace=direction_replace[2], mod=mod, string_for_count='def')
+                print(files_func(args))
+                args.string_for_search = args.string_for_replace = 'def'
             else:
                 break
 
--- test/test_files.py
+++ test/test_files.py
@@ -1,16 +1,88 @@
 import unittest
+from unittest.mock import patch
 import argparse
 from app.files import files_func
 
 
-class TestAnalytics(unittest.TestCase):
+# noinspection PyMethodMayBeStatic,PyMethodMayBeStatic
+class TestFiles(unittest.TestCase):
+    class FileObject:
 
-    EXPECT = ('YES', 'NO')
+        def __init__(self, string=None):
+            self.value = string
+            self.was_read_called = False
 
-    # def test_analytics_correct_params1(self):
-    #     args = argparse.Namespace(envelope_a='5', envelope_b='5', envelope_c='4', envelope_d='4')
-    #     self.assertEqual(analytics_func(args), self.EXPECT[0])
+        def __enter__(self):
+            return self
 
+        def __exit__(self, exc_type, exc_val, exc_tb):
+            pass
+
+        def read(self, *ignore):
+            if not self.was_read_called:
+                self.was_read_called = True
+                return self.value
+            else:
+                return False
+
+        def write(self, data):
+            if self.value:
+                self.value += data
+            else:
+                self.value = data
+
+        def get_value(self):
+            return self.value
+
+    def mock_open(*ignore):
+        return TestFiles.FileObject(
+            'aaaa nice aaa aa aaaaaaaaakka aaaaa nice')
+
+    # TEST COUNT
+    @patch('builtins.open', mock_open)
+    def test_files_correct_params_mod_c(self):
+        args = argparse.Namespace(files_name='*ignore', mod='c', string_for_count='aa')
+        self.assertEqual(files_func(args), 18)
+
+    @patch('builtins.open', mock_open)
+    def test_files_correct_params_not_existing_string(self):
+        args = argparse.Namespace(files_name='*ignore', mod='c', string_for_count='ffff')
+        self.assertEqual(files_func(args), 0)
+
+    @patch('builtins.open', mock_open)
+    def test_files_incorrect_params_default(self):
+        args = argparse.Namespace(files_name='*ignore', mod='c', string_for_count='def',
+                                  string_for_replace='def', string_for_search='def')
+        self.assertEqual(files_func(args), False)
+
+    def test_files_incorrect_params_file_not_found(self):
+        args = argparse.Namespace(files_name='testcount123', mod='c', string_for_count='aa')
+        self.assertEqual(files_func(args), 'File Not Found')
+
+    @patch('builtins.open', mock_open)
+    def test_files_incorrect_params_None(self):
+        args = argparse.Namespace(files_name='*ignore', mod=None, string_for_count='aa')
+        self.assertEqual(files_func(args), False)
+
+    # TEST REPLACE
+    RESULT_STRING = 'aaaa hi aaa aa aaaaaaaaakka aaaaa hi'
+
+    @patch('builtins.open', mock_open)
+    def test_files_correct_params_mod_r(self):
+        args = argparse.Namespace(files_name='*ignore', mod='r', string_for_search='nice', string_for_replace='hi',
+                                  string_for_count='def')
+        self.assertEqual(files_func(args), 'Done. Check your file')
+
+    @patch('builtins.open', mock_open)
+    def test_files_correct_params_string_not_found(self):
+        args = argparse.Namespace(files_name='*ignore', mod='r', string_for_search='hello', string_for_replace='hi',
+                                  string_for_count='def')
+        self.assertEqual(files_func(args), 'The string is not found')
+
+    def test_files_correct_params_file_not_found(self):
+        args = argparse.Namespace(files_name='file12', mod='r', string_for_search='nice', string_for_replace='hi',
+                                  string_for_count='def')
+        self.assertEqual(files_func(args), 'File Not Found')
 
 
 if __name__ == 'main':
